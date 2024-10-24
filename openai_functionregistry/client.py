1251a6e4306eb49cb0cefafef4fa757945c33976
Torstein Sørnes <t.soernes@gmail.com>
2024-10-24 09:28:03 +0200

    Factor out cost calculation

--- openai_functionregistry/client.py
+++ openai_functionregistry/client.py
@@ -1,4 +1,5 @@
 import os
+import re
 
 from dotenv import load_dotenv
 from typing import Iterable
@@ -109,6 +110,11 @@ class Client:
         if api_version is None:
             self.api_version = os.getenv("OAI-GPT4O-API-VERSION", "2024-08-01-preview")
 
+        if model_date := re.search(r"(\d\d\d\d\d\d\d\d)", self.model):
+            self.model_version = datetime.datetime.strptime(model_date.group(1), "%d%m%Y")
+        else:
+            raise ValueError(f"Could not detect model version {self.model}")
+
         self.is_mini = is_mini
         self.client = AzureOpenAI(
             azure_endpoint=self.azure_endpoint,
@@ -125,38 +131,57 @@ class Client:
 
         https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/
         """
-        if self.is_mini:
-            # GPT-4o-mini Global Deployment
-            cost_per_1m_inp_nok = 1.57738
-            cost_per_1m_out_nok = 6.3095
-        elif datetime.datetime.strptime(self.api_version[:10], "%Y-%m-%d") >= datetime.datetime(year=2024, month=8, day=6):
-            # gpt-4o-2024-08-06 Global Deployment
-            cost_per_1m_inp_nok = 26.2896
-            cost_per_1m_out_nok = 105.158001
-        else:
-            # GPT-4o Global Deployment
-            cost_per_1m_inp_nok = 52.5791
-            cost_per_1m_out_nok = 157.7371
+        return calculate_cost(is_mini=self.is_mini, model_version=self.model_version, input_tokens=input_tokens, output_tokens=output_tokens)
 
-        encoder = tiktoken.encoding_for_model("gpt-4o")  # same encoding with 4o-mini as with 4o
-        if isinstance(input_tokens, int):
-            n_input_tokens = input_tokens
-        elif isinstance(input_tokens, str):
-            n_input_tokens = len(encoder.encode(input_tokens))
-        elif isinstance(input_tokens, Iterable):
-            n_input_tokens = sum(map(len, encoder.encode_batch(list(input_tokens))))
-        else:
-            raise TypeError(input_tokens)
-        if isinstance(output_tokens, int):
-            n_output_tokens = output_tokens
-        elif isinstance(output_tokens, str):
-            n_output_tokens = len(encoder.encode(output_tokens))
-        elif isinstance(output_tokens, list):
-            n_output_tokens = sum(map(len, encoder.encode_batch(output_tokens)))
-        else:
-            raise TypeError(output_tokens)
+def calculate_cost(is_mini: bool, model_version: str | datetime.datetime, input_tokens: str |  Iterable[str] | int = 0, output_tokens: str | Iterable[str] | int = 0) -> LLMCost:
+    """
+    tokens: a text string, a list of text strings, or the number of tokens (int)
+    Returns a LLM cost object.
 
-        mil = 1_000_000
-        input_cost = n_input_tokens * cost_per_1m_inp_nok / mil
-        output_cost = n_output_tokens * cost_per_1m_out_nok / mil
-        return LLMCost(n_input_tokens=n_input_tokens, input_cost=input_cost, n_output_tokens=n_output_tokens, output_cost=output_cost, currency="NOK")
+    model_version: "%d%m%Y"
+
+    In NOK.
+
+    https://azure.microsoft.com/en-us/pricing/details/cognitive-services/openai-service/
+    """
+    if isinstance(model_version, str):
+        if model_date := re.search(r"(\d\d\d\d\d\d\d\d)", model_version):
+            model_version = datetime.datetime.strptime(model_date.group(1), "%d%m%Y")
+        else:
+            raise ValueError(f"Could not detect model version {model_version}")
+
+    if is_mini:
+        # GPT-4o-mini Regional API
+        cost_per_1m_inp_nok = 1.735108
+        cost_per_1m_out_nok = 6.94043
+    elif model_version >= datetime.datetime(year=2024, month=8, day=6):
+        # gpt-4o-2024-08-06 Global Deployment
+        cost_per_1m_inp_nok = 26.2896
+        cost_per_1m_out_nok = 105.158001
+    else:
+        # GPT-4o Global Deployment
+        cost_per_1m_inp_nok = 52.5791
+        cost_per_1m_out_nok = 157.7371
+
+    encoder = tiktoken.encoding_for_model("gpt-4o")  # same encoding with 4o-mini as with 4o
+    if isinstance(input_tokens, int):
+        n_input_tokens = input_tokens
+    elif isinstance(input_tokens, str):
+        n_input_tokens = len(encoder.encode(input_tokens))
+    elif isinstance(input_tokens, Iterable):
+        n_input_tokens = sum(map(len, encoder.encode_batch(list(input_tokens))))
+    else:
+        raise TypeError(input_tokens)
+    if isinstance(output_tokens, int):
+        n_output_tokens = output_tokens
+    elif isinstance(output_tokens, str):
+        n_output_tokens = len(encoder.encode(output_tokens))
+    elif isinstance(output_tokens, list):
+        n_output_tokens = sum(map(len, encoder.encode_batch(output_tokens)))
+    else:
+        raise TypeError(output_tokens)
+
+    mil = 1_000_000
+    input_cost = n_input_tokens * cost_per_1m_inp_nok / mil
+    output_cost = n_output_tokens * cost_per_1m_out_nok / mil
+    return LLMCost(n_input_tokens=n_input_tokens, input_cost=input_cost, n_output_tokens=n_output_tokens, output_cost=output_cost, currency="NOK")
